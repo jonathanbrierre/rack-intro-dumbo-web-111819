83db86d5e185e1394cec023646807227fd0a4f92
Jonathan Brierre <github email address>
2019-12-07 19:47:27 -0500

    Done.

--- application.rb
+++ application.rb
@@ -3,8 +3,13 @@ class Application
   def call(env)
     resp = Rack::Response.new
     resp.write "Hello, World"
+    resp.write "Hello, my name is"
     resp.finish
   end
+
+  # def introduction
+  #   return [ 200, {'Content-Type' => 'text/html'}, [ "Hello, my name is" ] ]
+  # end
 
 end
 
